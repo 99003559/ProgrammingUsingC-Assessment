4a1d294cbe2fe3224cc786a8ab0d5d4dce8920c5
99003559 <78849822+99003559@users.noreply.github.com>
2021-02-25 17:30:29 +0530

    Update GourmetOrganicShop.cs

--- ProblemThree_CompleteSolution_BL/GourmetOrganicShop.cs
+++ ProblemThree_CompleteSolution_BL/GourmetOrganicShop.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +6,36 @@ using System.Threading.Tasks;
 
 namespace ProblemThree_CompleteSolution_BL
 {
-    public class GourmetOrganicShop : GourmetShop,ITaxCompute
+    public class GourmetOrganicShop : GourmetShop, ITaxCompute
     {
-        
+        public GourmetOrganicShop(int v1, string v2, Dictionary<int, double> organicPriceDict)
+        {
+            V1 = v1;
+            V2 = v2;
+            OrganicPriceDict = organicPriceDict;
+        }
+
+        public int V1 { get; }
+        public string V2 { get; }
+        public Dictionary<int, double> OrganicPriceDict { get; }
+        public int ItemName { get; set; }
+
+        public override double PayPerPiece(int quantity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override double PayPerWeight(int weight)
+        {
+            throw new NotImplementedException();
+        }
+
+        public double TaxOnTotalBill()
+        {
+            throw new NotImplementedException();
+        }
+
+      
+
     }
 }
