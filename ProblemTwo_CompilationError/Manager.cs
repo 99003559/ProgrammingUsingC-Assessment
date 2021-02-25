9a82bed5f003770d04e98b2f6f5a5b383113d820
99003559 <78849822+99003559@users.noreply.github.com>
2021-02-25 16:34:40 +0530

    Update Manager.cs

--- ProblemTwo_CompilationError/Manager.cs
+++ ProblemTwo_CompilationError/Manager.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,16 +6,34 @@ using System.Threading.Tasks;
 
 namespace ProblemTwo_CompilationError
 {
-    class Manager
+   public class Manager :Employee
     {
+        private static int eID;
+        private static string eName;
+        private static int eLevel;
+        private static DateTime eDoJ;
+        private List<Employee> lstDirectReports;
+        private string v;
+
         public List<Employee> DirectReports { get; set; }
         public string CostCode { get; set; }
+        public int EmpID { get; set; }
+        public string EmpName { get; set; }
+        public int EmpLevel { get; set; }
+
 
         public Manager(List<Employee> drEmployeesLst, string cCode) :base(eID, eName, eLevel, eDoJ)
         {
+            EmpID = eID;
             DirectReports = drEmployeesLst;
             CostCode = cCode;
 
+        }
+
+        public Manager(int eID, string eName, int eLevel, DateTime eDoJ, List<Employee> lstDirectReports, string v) : base(eID, eName, eLevel, eDoJ)
+        {
+            this.lstDirectReports = lstDirectReports;
+            this.v = v;
         }
 
         public List<string> GetMyReports()
@@ -25,3 +43,4 @@ namespace ProblemTwo_CompilationError
 
     }
 }
+
